53b71b0bfc2ba8137166ab06e7dcc6534b61ad73
igor <igoratakhanov@gmail.com>
2019-11-12 13:59:43 -0500

    counterMaker() passing using closure

--- index.js
+++ index.js
@@ -264,9 +264,10 @@ function tallyUpDonations(runners) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
-  function counter() {
-    ++count
+  let count = -1
+  return function counter() {
+    count++
+    return count
   }
   // BROKEN CODE ENDS
 }
