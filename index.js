c6761c1bd7802101ede998cf7c4234611be6db2d
igor <igoratakhanov@gmail.com>
2019-11-12 13:24:39 -0500

    firstNamesAllCaps() passing

--- index.js
+++ index.js
@@ -209,8 +209,10 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  const firstNames = []
+  runners.forEach(runner => firstNames.push(runner.first_name.toUpperCase()))
+  return firstNames
 }
 
 /**
